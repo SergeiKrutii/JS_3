fe3e1c20ebf196afeaf338cf8d508fa456002bd2
SergeiKrutii <106181150+SergeiKrutii@users.noreply.github.com>
2022-12-08 21:27:27 +0200

    rework with delegation

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,25 +4,27 @@ const refs = {
     form: document.querySelector('.feedback-form'), 
     input: document.querySelector('input'),
     textarea: document.querySelector('textarea'),
-    button: document.querySelector('button')
+    
 }
 const STORAGE_KEY = "feedback-form-state";
 let formData = {}
 
+
 refs.form.addEventListener('input', throttle(onFormInput, 500))
-refs.button.addEventListener('submit', onFormSubmit)
+refs.form.addEventListener('submit', onFormSubmit)
 
 onSavedValue()
 
 function onFormInput(e) {
     formData[e.target.name] = e.target.value;
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-
+    
 }
 
 function onSavedValue() {
     try {
         const { message, email } = JSON.parse(localStorage.getItem(STORAGE_KEY));
+        
         if (STORAGE_KEY) {
             refs.textarea.value = message;
             refs.input.value = email;
@@ -33,9 +35,9 @@ function onSavedValue() {
     }
 }
 
-
 function onFormSubmit(e) {
     e.preventDefault();
-    e.currentTarget.reseset();
+    console.log(formData)
+    e.currentTarget.reset();
     localStorage.removeItem("feedback-form-state");
 }
